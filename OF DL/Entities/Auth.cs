312236334ab9ec55e766f4e40ab4f83d9d591ed5
Silvered Gold <35001040+silveredgold@users.noreply.github.com>
2023-08-08 11:55:24 +1000

    Add option to skip posts with ads/trials;

--- OF DL/Entities/Auth.cs
+++ OF DL/Entities/Auth.cs
@@ -27,6 +27,7 @@ namespace OF_DL.Entities
         public bool DownloadVideos { get; set; }
         public bool DownloadAudios { get; set; }
         public bool IncludeExpiredSubscriptions { get; set; }
+        public bool SkipAds { get; set; } = false;
         public string? DownloadPath { get; set; } = string.Empty;
 
     }
