5ebaf364a391dfbee2aab58e023184063a8a4d36
sim0n00ps <simoncoops64@gmail.com>
2024-06-05 20:49:38 +0100

    Change dynamic rules URL

--- OF DL/Helpers/Constants.cs
+++ OF DL/Helpers/Constants.cs
@@ -3,5 +3,5 @@ namespace OF_DL.Helpers;
 public static class Constants
 {
     public const string API_URL = "https://onlyfans.com/api2/v2";
-    public const string DYNAMIC_RULES = "https://raw.githubusercontent.com/SneakyOvis/onlyfans-dynamic-rules/main/rules.json";
+    public const string DYNAMIC_RULES = "https://raw.githubusercontent.com/Growik/onlyfans-dynamic-rules/main/rules.json";
 }
