0b369fe5a086310a1131de8dee0a48dba9af324e
whimsical-c4lic0 <chilling@cozycalico.cafe>
2025-02-15 13:27:25 -0600

    Add novnc to docker image to allow interaction with new auth workflow

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,10 +2,6 @@
 
 mkdir -p /config/cdm/devices/chrome_1610
 
-if [ ! -f /config/auth.json ]; then
-	cp /default-config/auth.json /config/auth.json
-fi
-
 if [ ! -f /config/config.json ]; then
 	cp /default-config/config.json /config/config.json
 fi
@@ -13,5 +9,9 @@ fi
 if [ ! -f /config/rules.json ]; then
 	cp /default-config/rules.json /config/rules.json
 fi
+
+{
+  supervisord -c /etc/supervisor/conf.d/supervisord.conf &
+} &> /dev/null
 
 /app/OF\ DL
