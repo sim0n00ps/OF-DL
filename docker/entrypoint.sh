2315f10ef34715c56e9f42c0b561a4d5eea3f695
whimsical-c4lic0 <chilling@cozycalico.cafe>
2025-03-30 00:40:56 -0500

    Create logs directory if it doesn't exist to prevent supervisord from crashing

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mkdir -p /config/cdm/devices/chrome_1610
+mkdir -p /config/logs/
 
 if [ ! -f /config/config.conf ] && [ ! -f /config/config.json ]; then
 	cp /default-config/config.conf /config/config.conf
